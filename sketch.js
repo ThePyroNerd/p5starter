71b3bad06476b79c04268da9461ae3b6e6c72600
Cameron <cameron@thepyronerd.com>
2021-03-02 20:17:10 -0600

    Basic functionality complete.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
+// https://p5js.org/reference/
+let sound, amplitude;
 
+function preload(){
+  sound = loadSound('music.mp3');
+}
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  let cnv = createCanvas(displayWidth, displayHeight);
+  cnv.mouseClicked(toggleSound);
+  amplitude = new p5.Amplitude();
 }
 
 function draw() {
-  background(220);
-  ellipse(50,50,80,80);
- 
+  background('black');
+  let level = amplitude.getLevel();
+  fill('crimson')
+  ellipse(150, 150,level * 150);
+  fill('blue')
+  ellipse(250,300,level * 300);
 }
 
-
-
+function toggleSound(){
+  if (sound.isPlaying()) {
+    sound.stop();
+  } else {
+    sound.play();
+  }
+}
